4b04ca829d3e090048dddee757e55211f2739096
rusakovic <maxrusakovich@gmail.com>
2020-03-07 13:51:33 +0300

    fething random region

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useCallback } from 'react'
+import axios from 'axios'
 
-function App() {
+import './App.css'
+
+const App = () => {
+  const REGIONS_API_URL = `http://apiv3.iucnredlist.org/api/v3/region/list?token=${process.env.REACT_APP_RED_LIST_API_TOKEN}`
+  console.log(process.env.RED_LIST_API_TOKEN)
+  const [randomRegion, setRandomRegion] = useState({
+    region: '',
+    isFetching: false
+  })
+
+  // 1. Load the list of the available regions for species
+  const fetchSpeciousRegions = useCallback(async () => {
+    try {
+      setRandomRegion({
+        randomRegion,
+        isFetching: true
+      })
+      const response = await axios.get(REGIONS_API_URL)
+      const regionsArray = response.data.results
+
+      // 2. Take a random region from the list
+      const selectedRandomRegion =
+        regionsArray[Math.floor(Math.random() * regionsArray.length)]
+      setRandomRegion({
+        region: selectedRandomRegion.identifier,
+        isFetching: false
+      })
+    } catch (error) {
+      console.log(error)
+      setRandomRegion({
+        randomRegion,
+        isFetching: false
+      })
+    }
+  }, [])
+
+  useEffect(() => {
+    fetchSpeciousRegions()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h3>RANDOM REGION</h3>
+      {randomRegion.isFetching ? (
+        <h5>Loading...</h5>
+      ) : (
+        <h5>{randomRegion.region}</h5>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
